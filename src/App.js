5b7376ea7eb7f218257827cf6b85c8a6f22802fa
anpdt <sajno076@gmail.com>
2022-05-21 15:35:35 +0700

    test api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React from "react";
+const axios = require("axios").default;
 
 function App() {
+  axios.get("/api/public/hello").then(function (response) {
+    // handle success
+    console.log(response);
+  });
   return <div className="App"></div>;
 }
 
