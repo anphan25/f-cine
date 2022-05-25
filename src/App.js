c1bdd9133e73e69ff62c103c0f06571e555becc2
anpdt <sajno076@gmail.com>
2022-05-25 08:57:01 +0700

    text api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { CollapseDrawerProvider } from 'contexts/CollapseDrawerContext';
-import React from 'react';
-import Router from './routes/Routes';
-const axios = require('axios').default;
+import { CollapseDrawerProvider } from "contexts/CollapseDrawerContext";
+import React from "react";
+import Router from "./routes/Routes";
+const axios = require("axios").default;
 
 function App() {
-  // axios.get('/api/public/hello').then(function (response) {
-  //   // handle success
-  //   console.log(response);
-  // });
+  axios.get("/api/public/hello").then(function (response) {
+    // handle success
+    console.log(response);
+  });
   return (
     <CollapseDrawerProvider>
       <Router />
