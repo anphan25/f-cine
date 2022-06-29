c299bea3451b4f3f20fdb0f7161982372f933b6b
dung170920 <dungnguyen09172000@gmail.com>
2022-06-29 15:55:25 +0700

    Call api for feature add tickets

---
+++ src/services/TicketService.js
@@ -0,0 +1,7 @@
+import { axiosPrivate } from "utils/axiosConfig";
+
+const apiPath = "/tickets";
+
+export const postTickets = async (params) => {
+  return await axiosPrivate.post(apiPath, params);
+};
