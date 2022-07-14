575c641f03dfc9b190db9ff7095f250a2abf53e9
Doan Pham Bich Hop <hopdoan.work@gmail.com>
2022-07-14 22:37:29 +0700

    404 page

---
+++ src/pages/message/404.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import NotFoundImage from "../../assets/images/page-not-found.gif";
+import style from "./404.module.css";
+
+const PageNotFound = () => {
+  return (
+    <div className={style.center}>
+      <img src={NotFoundImage} alt="404" />
+      <div>
+        <h1 className={style.block}> Sorry something went wrong </h1>
+        <h1 className={style.block}>
+          Please back to {""}
+          <span>
+            <Link to="/" className={style.home}>
+              Home Page
+            </Link>
+          </span>
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default PageNotFound;
